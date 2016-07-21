cc85f0f7f7f3c065c8e7d8075f022e9160b11dd3
KouKariya <KouKariya@users.noreply.github.com>
2016-07-21 14:27:50 -0500

    Update decoder.py
    Updated data types to make parsing the binary number easier.

--- decoder.py
+++ decoder.py
@@ -1,15 +1,14 @@
 #decoder.py
 #Written by Jesse Gallarzo
 
-binInput=input("Enter 4 bit binary number: ")
-binValue=1
+binInput=str(input("Enter 4 bit binary number:"))
+binValue =1
 result=int()
 
-for num in range(len(binInput)):
-  if(num==0):
-    result+=binValue
-    binValue*= 2
-  else():
-    binValue*=2
-    
+for num in binInput:
+	if(num=="1"):
+		result+=binValue
+		binValue*=2
+	elif(num=="0"):
+		binValue*=2
 print(result)
