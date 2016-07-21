eedddbde3a0cc90fb5ea91d61839ae6bdfd0073d
KouKariya <KouKariya@users.noreply.github.com>
2016-07-21 13:21:29 -0500

    Update decoder.py
    Created script that allows the decoding of 4 bit binary numbers

--- decoder.py
+++ decoder.py
@@ -1,2 +1,15 @@
 #decoder.py
 #Written by Jesse Gallarzo
+
+binInput=input("Enter 4 bit binary number: ")
+bin=1
+value = int()
+
+for num in range(len(binInput)):
+  if(num==0):
+    value+=bin
+    bin*= 2
+  else():
+    bin*=2
+    
+  print(value)
