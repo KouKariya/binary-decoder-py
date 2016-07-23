b6a137bd55491a4754585b649b54ef58d71f48a4
KouKariya <KouKariya@users.noreply.github.com>
2016-07-23 08:46:20 -0500

    Update decoder.py
    Updated proper data type to parse binary value better.

--- decoder.py
+++ decoder.py
@@ -1,14 +1,14 @@
 #decoder.py
 #Written by Jesse Gallarzo
 
-binInput=str(input("Enter 4 bit binary number:"))
+binInput=str(input("Enter 4 bit binary number: "))
 binValue =1
-result=int()
+result=0
 
 for num in binInput:
 	if(num=="1"):
 		result+=binValue
 		binValue*=2
-	elif(num=="0"):
+	else(num=="0"):
 		binValue*=2
 print(result)
