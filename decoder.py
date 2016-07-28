2c889505f85f19ea67a8632952e1334ba28de7f1
KouKariya <KouKariya@users.noreply.github.com>
2016-07-28 00:39:29 -0500

    Update decoder.py
    Added comments for future implementations.

--- decoder.py
+++ decoder.py
@@ -1,7 +1,9 @@
 #decoder.py
 #Written by Jesse Gallarzo
+#add commented code to implement this script in Python 3
 
 binInput=raw_input("Enter 4 bit binary number: ")
+#reverse string to account for right-to-left from binary values
 binValue =1
 result=0
 
